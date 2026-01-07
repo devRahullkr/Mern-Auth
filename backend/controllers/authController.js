8897e2ead49a2c9425d42ca289a75b32a4d5ef38
Rahulllkr <rahulkumar8604745@gmail.com>
2026-01-07 06:13:27 +0530

    signup and login

---
+++ backend/controllers/authController.js
@@ -0,0 +1,79 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async(req,res) => {
+    
+    const {name,email,password} = req.body;
+
+    if(!name || !email || !password){
+        return res.status(400).json({success:false,message:"Missing Details"})
+    }
+
+    try {
+
+        const existingUser = await userModel.findOne({email})
+
+        if(existingUser){
+            return res.json({success:false,message:"User already exists"})
+        }
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        const user = new userModel({name,email,password:hashedPassword})
+
+        await user.save()
+
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET_KEY,{expiresIn:"7d"})
+
+        res.cookie("token",token,
+            {
+                httpOnly:true,
+                secure:process.env.NODE_ENV === "production",
+                sameSite:process.env.NODE_ENV === "production" ? "none" : "strict",
+                maxAge:7*24*60*60*1000
+            })
+        
+    } catch (err) {
+        res.json({success:false,message:err.message})
+    }
+
+}
+
+
+export const login = async(req,res) => {
+
+    const {email,password} = req.body;
+
+    if(!email || !password){
+        return res.json({success:false,message:"Email and Password required"})
+    }
+
+    try {
+
+        const user = await userModel.findOne({email})
+
+        if(!user){
+            return res.json({success:false,message:"Invalid Email"})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if(!isMatch){
+            return res.json({success:false,message:"Invalid Password"})
+        }
+
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET_KEY,{expiresIn:"7D"})
+
+        res.cookie("token",token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV === "production",
+            sameSite:process.env.NODE_ENV === "production" ? "none" : "strict",
+            maxAge:7*24*60*60*1000
+        })
+
+        return res.json({success:true})
+        
+    } catch (err) {
+        return res.json({success:false,message:err.message})
+    }
+}
